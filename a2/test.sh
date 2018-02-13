cf9a18a2fc2d05888728bf3ebc7a9224634b21ce
ABHASH JAIN <ajain28@ncsu.edu>
2018-02-13 01:28:22 -0500

    Test Script

---
+++ a2/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+rm final.txt
+rm student.db;rm index.bin;rm avail.bin
+gcc assign_2.c -o a -Wall -g
+./a  --first-fit student.db < input-01.txt > o.txt
+diff o.txt output-01-first.txt >> final.txt
+./a  --first-fit student.db < input-02.txt > o.txt
+diff o.txt output-02-first.txt >> final.txt
+
+rm student.db;rm index.bin;rm avail.bin
+echo "Best test case" >>final.txt
+./a  --best-fit student.db < input-01.txt > o.txt
+diff o.txt output-01-best.txt >> final.txt
+./a  --best-fit student.db < input-02.txt > o.txt
+diff o.txt output-02-best.txt >> final.txt
+
+rm student.db;rm index.bin;rm avail.bin
+echo "worst test case" >>final.txt
+./a  --worst-fit student.db < input-01.txt > o.txt
+diff o.txt output-01-worst.txt >> final.txt
+./a  --worst-fit student.db < input-02.txt > o.txt
+diff o.txt output-02-worst.txt >> final.txt
